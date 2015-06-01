d590bdbf41d20b0c210f70a0983f6fe0e1c25e9d
meteor5118 <free.meteor@163.com>
2015-06-01 23:00:02 +0800

    Update dbutils.py

--- util/dbutils.py
+++ util/dbutils.py
@@ -1,1 +1,9 @@
 #
+def save_log(log):
+    """log is dict """
+    db.set('log_table:%s:business_id' % log['user_id'], log['business_id'])
+    db.set('log_table:%s:type' % log['user_id'], log['type'])
+    db.set('log_table:%s:mark' % log['user_id'], log['mark'])
+    db.set('log_table:%s:timestamp' % log['user_id'], log['timestamp'])
+    
+    db.set('log_table:%s:business_id' % log['user_id'])
