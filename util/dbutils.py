4d6ee86df5cdeb59366b9975e96273d945a37881
meteor5118 <free.meteor@163.com>
2015-06-02 17:30:15 +0800

    Update dbutils.py
    save log_table

--- util/dbutils.py
+++ util/dbutils.py
@@ -1,9 +1,32 @@
-#
+import redis
+import logging, os
+
+db = None
+log_id = 0
+
 def save_log(log):
-    """log is dict """
-    db.set('log_table:%s:business_id' % log['user_id'], log['business_id'])
-    db.set('log_table:%s:type' % log['user_id'], log['type'])
-    db.set('log_table:%s:mark' % log['user_id'], log['mark'])
-    db.set('log_table:%s:timestamp' % log['user_id'], log['timestamp'])
-    
-    db.set('log_table:%s:business_id' % log['user_id'])
+    global log_id
+    global db
+
+    #save log_table
+    log_id += 1
+    db.set('log_table:%d:user_id' % log_id, log['user_id'])
+    db.set('log_table:%d:business_id' % log_id, log['business_id'])
+    db.set('log_table:%d:type' % log_id, log['type'])
+    db.set('log_table:%d:mark' % log_id, log['mark'])
+    db.set('log_table:%d:timestamp' % log_id, log['timestamp'])
+
+    #update user_type_table
+    db.set('user_type_table:%s' )
+
+    #update user_type_weight_table
+    db.set()
+
+    #update business_type_table
+    db.set()
+
+
+if __name__ == '__main__':
+    db = redis.Redis('127.0.0.1', '6379')
+    log = {'user_id': '123', 'business_id':'67348367', 'type':'1', 'mark':'8.5', 'timestamp':'2015-06-02 16:40:40'}
+    save_log(log)
